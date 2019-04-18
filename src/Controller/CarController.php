f1a4a2e564223e10707a8ef8e491cdcbe37e16c8
WebjobsBiz <mitke013@gmail.com>
2019-04-18 22:06:28 +0200

    Bug

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -9,23 +9,14 @@ use App\Entity\CarReview;
 use App\EventSubscriber\KernelReset\EMResetter;
 use App\Repository\CarRepository;
 use App\Repository\CarReviewRepository;
-use function array_merge;
 use HTC\SpaBundle\Annotation\Outlet;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\DependencyInjection\ServicesResetter;
 use Symfony\Component\Routing\Annotation\Route;
-use function var_dump;
 
 class CarController extends AbstractController
 {
-    public static function getSubscribedServices()
-    {
-        return array_merge(parent::getSubscribedServices(), [
-            'services_resetter' =>'services_resetter',
-        ]);
-    }
 
     /**
      * @Route("/", name="default")
@@ -34,10 +25,6 @@ class CarController extends AbstractController
      */
     public function index(CarRepository $carRepository, EMResetter $EMResetter): Response
     {
-        /** @var ServicesResetter $resetter */
-//        $resetter = $this->get('services_resetter');
-//        var_dump($resetter);
-
         return $this->render('default/index.html.twig', [
             'cars' => $carRepository->findAll(),
         ]);
